32f6c654106c9bfb67173786de14676ec3684848
MarcSan18 <marcsan18@hotmail.com>
2022-05-17 21:53:01 -0300

    configurando o express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("teste")
+const express = require('express');
+
+const app = express();
+
+const usuarios = [{
+    nome: 'Marcelo',
+    idade: 42,
+}];
+
+app.get('/usuarios', (request, response) => {
+    const usuarioNovo = usuarios.push(nome, idade)
+
+    response.json(usuarios);
+});
+
+app.listen(3000);
